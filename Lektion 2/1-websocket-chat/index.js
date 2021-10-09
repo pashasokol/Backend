164c6fadde2ccb381101e05f2e118f255b16dfde
Pavel <sokoliklive@gmail.com>
2021-10-09 15:00:20 +0200

    lektion 2

---
+++ Lektion 2/1-websocket-chat/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { listenerCount } = require('process');
+const socket = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, ()  => {
+    console.log('Server running on http://localhost: ' + PORT);
+})
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('new user connected', socket.id);
+
+
+    socket.emit('message', 'Welcome to the chat');
+
+
+})
+
+
+
+
